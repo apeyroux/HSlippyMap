fc8dd12576006bbb4c5a69685f04ca12e9136ba5
Jean-Alexandre Peyroux <ja@px.io>
2013-12-20 11:53:05 +0000

    change main for exemple

--- hsl.hs
+++ hsl.hs
@@ -1,8 +1,7 @@
 -- HSlippyMap Exemple
 import HSlippyMap
 
-main =
-  mapM (print . show . tileFromLatLong lat long) [0..18]
+main = mapM (print . show . tileFromLatLong lat long) [0..18]
   where
       lat = 48.8152
       long = 2.2712
